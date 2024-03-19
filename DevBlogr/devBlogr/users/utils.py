af316fbcb5c4414eb25c51d9d23eb8a8dd85f29a
Torpeedofv <torpeedo.fv@gmail.com>
2024-03-19 22:40:36 +0100

    rectified sender error

--- DevBlogr/devBlogr/users/utils.py
+++ DevBlogr/devBlogr/users/utils.py
@@ -21,8 +21,8 @@ def save_picture(form_picture):
 
 def send_reset_email(user):
     token = user.get_reset_token()
-    sender = Config.MAIL_USERNAME
-    msg = Message('Password Reset Request', sender=sender, recipients=[user.email])
+    
+    msg = Message('Password Reset Request', sender=Config.MAIL_USERNAME, recipients=[user.email])
     msg.body = f'''To reset your password, visit the following link: {url_for('users.reset_token', token=token, _external=True)}
 
     If you did not make this request then simply ignore this email and no changes will be made.
